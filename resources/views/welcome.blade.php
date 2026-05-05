013a2b45ee56faaa6b712177bb90fd1d739cec95
emilp780-star <emilp780@gmail.com>
2026-05-05 23:13:03 +0800

    Initial Commit - Laporan Instalasi Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Praktikum Laravel</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f4ff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .card {
+            background: white;
+            padding: 40px;
+            border-radius: 12px;
+            box-shadow: 0 4px 15px rgba(0,0,0,0.1);
+            text-align: center;
+        }
+        h1 { color: #4f46e5; }
+        p { color: #555; }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1> Welcome To Project Laporan 1</h1>
+        <p>Ini adalah halaman buatan saya untuk Laporan 1</p>
+        <p><strong>Nama:</strong> Emil Melinda Putri </p>
+        <p><strong>NIM:</strong> 60200124095 </p>
+    </div>
+</body>
+</html>
